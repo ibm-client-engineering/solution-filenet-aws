877de5009e7a4b5856bdc66d5513322363ccc14c
faraz <95059+faraz@users.noreply.github.com>
2023-04-30 23:45:00 -0400

    additional modules and css settings

---
+++ website/src/theme/ExternalLink/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import type { Props } from '@theme/Icon/ExternalLink'
+
+import styles from './styles.module.css'
+
+export default function IconExternalLink({ width = 13.5, height = 13.5 }: Props): JSX.Element {
+  return <></>
+}
