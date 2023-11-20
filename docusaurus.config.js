cde041b39d49c11a5e18ce4df56a9ef15c7144b2
faraz <afaraz@protonmail.com>
2023-11-20 09:46:29 -0500

    Update docusaurus.config.js
    Signed-off-by: faraz <afaraz@protonmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,10 +18,10 @@ async function createconfig() {
     favicon: "img/favicon.ico",
 
     // Set the production url of your site here
-    url: "https://filenet-eks.thinkforward.work/",
+    url: "https://ibm-client-engineering.github.io",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/",
+    baseUrl: "/solution-filenet-aws/",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
