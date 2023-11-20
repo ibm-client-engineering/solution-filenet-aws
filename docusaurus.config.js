ccfd52e830c6e4a878bcdd435afbdb0e1682ffef
kramerro-ibm <102995992+kramerro-ibm@users.noreply.github.com>
2023-11-20 09:53:57 -0500

    Update docusaurus.config.js
    Signed-off-by: kramerro-ibm <102995992+kramerro-ibm@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ async function createconfig() {
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: "/solution-filenet-aws/",
+    
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
